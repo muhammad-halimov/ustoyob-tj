9c232c2d112ea743312ecee1b4c7590c964a17ef
muhammad-halimov <muhammadi.halimov@icloud.com>
2025-11-17 03:57:50 +0200

    style: minor refactor

--- src/Controller/Api/Filter/User/Personal/UpdateUserPhotoController.php
+++ src/Controller/Api/Filter/User/Personal/UpdateUserPhotoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Api\Filter\User\Personal;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,22 +22,27 @@ class UpdateUserPhotoController extends AbstractController
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         $allowedRoles = ["ROLE_ADMIN", "ROLE_CLIENT", "ROLE_MASTER"];
+        /** @var User $bearerUser */
         $bearerUser = $this->security->getUser();
         $imageFile = $request->files->get('imageFile');
+        /** @var User $user */
         $user = $this->userRepository->find($id);
 
         if (!array_intersect($allowedRoles, $bearerUser->getRoles()))
             return $this->json(['message' => 'Access denied'], 403);
 
-        if (!$user || $user !== $bearerUser)
-            return $this->json(["message' => 'User not found OR Ownership doesn't match"], 404);
+        if (!$user)
+            return $this->json(["message' => 'User not found"], 404);
+
+        if($user !== $bearerUser)
+            return $this->json(["message" => "Ownership doesn't match"], 404);
 
         $user->setImageFile($imageFile);
 
         $this->entityManager->persist($user);
         $this->entityManager->flush();
 
-        return new JsonResponse([
+        return $this->json([
             'id' => $user->getId(),
             'image' => $user->getImage(),
             'message' => 'Photo updated successfully'
