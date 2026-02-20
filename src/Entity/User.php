189069b6847825b998037a6f53e531e5ea8a1818
muhammad-halimov <muhammadi.halimov@icloud.com>
2026-02-20 22:35:59 +0200

    fix: minor fix

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -458,7 +458,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         'masters:read',
         'clients:read',
 
-        'user:public:read',
+//        'user:public:read',
     ])]
     private ?bool $atHome = null;
 
