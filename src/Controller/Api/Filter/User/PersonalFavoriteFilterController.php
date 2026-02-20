0a6b041580c954c36c66dde39edc3f20dea675e8
muhammad-halimov <muhammadi.halimov@icloud.com>
2026-02-20 23:09:38 +0200

    fix: minor fix

--- src/Controller/Api/Filter/User/PersonalFavoriteFilterController.php
+++ src/Controller/Api/Filter/User/PersonalFavoriteFilterController.php
@@ -6,19 +6,22 @@ use App\Entity\Extra\Favorite;
 use App\Entity\User;
 use App\Repository\User\FavoriteRepository;
 use App\Service\Extra\AccessService;
+use App\Service\Extra\LocalizationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class PersonalFavoriteFilterController extends AbstractController
 {
     public function __construct(
-        private readonly FavoriteRepository $favoriteRepository,
-        private readonly AccessService      $accessService,
-        private readonly Security           $security,
+        private readonly FavoriteRepository  $favoriteRepository,
+        private readonly LocalizationService $localizationService,
+        private readonly AccessService       $accessService,
+        private readonly Security            $security,
     ){}
 
-    public function __invoke(): JsonResponse
+    public function __invoke(Request $request): JsonResponse
     {
         /** @var User $bearerUser */
         $bearerUser = $this->security->getUser();
@@ -27,6 +30,21 @@ class PersonalFavoriteFilterController extends AbstractController
 
         /** @var Favorite $favorite */
         $favorite = $this->favoriteRepository->findUserFavorites($bearerUser)[0] ?? null;
+
+        $locale = $request->query->get('locale', 'tj');
+
+        foreach ($favorite->getTickets() as $ticket) {
+            $this->localizationService->localizeGeography($ticket, $locale);
+
+            if ($ticket->getCategory())
+                $this->localizationService->localizeEntity($ticket->getCategory(), $locale);
+
+            if ($ticket->getUnit())
+                $this->localizationService->localizeEntity($ticket->getUnit(), $locale);
+
+            if ($ticket->getSubcategory())
+                $this->localizationService->localizeEntity($ticket->getSubcategory(), $locale);
+        }
 
         return empty($favorite)
             ? $this->json(['message' => 'Resource not found'], 404)
