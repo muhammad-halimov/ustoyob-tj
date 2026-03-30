c52a565a13bc732cf2feb9d9204d304ea9cf40aa
muhammad-halimov <muhammadi.halimov@icloud.com>
2026-03-30 18:51:39 +0300

    fix: minor fix

--- src/Controller/Api/CRUD/POST/User/User/ApiPostGrantRoleController.php
+++ src/Controller/Api/CRUD/POST/User/User/ApiPostGrantRoleController.php
@@ -23,9 +23,9 @@ class ApiPostGrantRoleController extends AbstractApiPostController
         if (!in_array('ROLE_MASTER', $bearer->getRoles()) && in_array('ROLE_CLIENT', $bearer->getRoles()))
             return $this->errorJson(AppMessages::ROLE_ALREADY_CLIENT);
 
-        if ($dto->role == 'master')
+        if ($dto->role == 'master' || $dto->role == 'MASTER')
             $bearer->setRoles(['ROLE_MASTER']);
-        elseif ($dto->role == 'client')
+        elseif ($dto->role == 'client' || $dto->role == 'CLIENT')
             $bearer->setRoles(['ROLE_CLIENT']);
         else return $this->errorJson(AppMessages::WRONG_ROLE);
 
